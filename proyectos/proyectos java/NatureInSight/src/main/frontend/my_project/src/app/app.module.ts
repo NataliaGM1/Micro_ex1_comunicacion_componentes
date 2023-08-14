ceeaf19127a31416d3dda895923b38e182263d3e
NataliaGM1 <ngmihalache@gmail.com>
2023-08-14 15:18:23 +0200

    banner component

---
+++ proyectos/proyectos java/NatureInSight/src/main/frontend/my_project/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BannerComponent } from './shared/banner/banner.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    BannerComponent,
+    FooterComponent,
+    LoginComponent,
+    RegisterComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
